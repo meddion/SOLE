68099c9d1d82cb7db642c816b602ac13e6eb0bbf
flabsby <marianqwerty511@gmail.com>
2019-11-06 17:33:34 +0200

    slightly optimized

--- Core/Methods/successive_overrelaxation.cs
+++ Core/Methods/successive_overrelaxation.cs
@@ -27,54 +27,41 @@ namespace Core.Methods
         }
         public void sor_solver(Matrix a, Vector b)
         {
-            //init
             dimension = a.Size;
             x = new Vector(dimension);
             old_x = new Vector(dimension);
             int i;
             //int cnt = 0;
             double error = 1000;
-            //work
             for (i = 0; i < dimension; i++)
             {
                 x[i] = 0;
             }
             while (error > eps)
             {
-               // Console.WriteLine("{0}=============", ++cnt); //debug
-                //update old values
+              //  Console.WriteLine("{0}=============", ++cnt); //debug
                 for (i = 0; i < dimension; i++)
                 {
                     old_x[i] = x[i];
                 }
-                //solver
                 for (i = 0; i < dimension; i++)
                 {
                     double sigma = 0;
-                    for (int k = 0; k < dimension; k++)
-                    {
-                        
-                        if (k != i)
-                        {
-                            sigma = sigma + a[i, k] * x[k];
-                        }
-                    }
+                    for (int k = 0; k < dimension; k++) 
+                        if (k != i) sigma = sigma + a[i, k] * x[k];
+
                     gs = ((b[i]) - sigma)/a[i,i];
                     x[i] = (omega * gs)+((1 - omega) * (old_x[i])) ;
                 //    Console.WriteLine("{0} -> {1}", i + 1, x[i]); //debug
                 }
                 
-                //errors
                 error = find_max(x, old_x);
               //  Console.WriteLine("Error:\n{0}",error);//debug
             }
-            // naxyu ubrat'
            // Console.ForegroundColor = ConsoleColor.Green;
            // Console.WriteLine("\n======   DONE!!!   ======\n");
            // Console.ForegroundColor = ConsoleColor.White;
-           // show(x);
-            //daaa vertae vector a ne pokazatel' na tru pizdu
-            }         // main solver
+            } 
         private double find_max(Vector a, Vector b)
         {
             double max = 0;
@@ -83,20 +70,11 @@ namespace Core.Methods
                 if (Math.Abs(a[i] - b[i]) > max) max = Math.Abs(a[i] - b[i]);
             }
             return max;
-        }       // max_error
-        private void show(Vector arr)
-        {
-            Log?.NewMsg("Result:\n");
-            for (int i = 0; i < dimension; i++)
-            {
-                Log?.NewMsg(Convert.ToString(arr[i])+"\n");
-            }
-        }                    // nahuj ne nujno
-                                                           // <- kakogo xuya vonou krivo?
+        }      
         public successive_overrelaxation()
         {
             this.dimension = 0;
             this.gs = 0;
-        }             // nedokonstructor
+        }           
     }
 }
