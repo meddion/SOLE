0e1e5cbbe7a3856fdbc0d21f5ed2a962184a9afe
flabsby <marianqwerty511@gmail.com>
2019-11-10 22:50:38 +0200

    commit

--- Core/Methods/successive_overrelaxation.cs
+++ Core/Methods/successive_overrelaxation.cs
@@ -9,7 +9,7 @@ namespace Core.Methods
     public class successive_overrelaxation : IMethod
     {
         private const double eps = 1e-14;
-        private const double omega = 1.01;                              
+        private const double omega = 1.091;                              
         private double gs;              
         private int dimension;       
         private Vector x, old_x;    
@@ -24,44 +24,71 @@ namespace Core.Methods
             Log?.NewMsg("Lul, ne naebav))");
             return x;
         }
-
-
+        private bool isDiagDominant(int n,Matrix a)
+        {
+            int i, j;
+            Vector tmp = new Vector(n);
+            for (i = 0; i < n; i++)
+            {
+                double sum = 0;
+                for (j = 0; j < n; j++)
+                {
+                    sum += Math.Abs(a[i, j]);
+                }
+                sum -= Math.Abs(a[i, i]);
+                if (Math.Abs(a[i, i]) < sum)
+                    return false;
+            }
+            return true;
+        }
         public void sor_solver(Matrix a, Vector b)
         {
+            int i, maxiter = 150,cnt=0;
             dimension = a.Size;
             x = new Vector(dimension);
             old_x = new Vector(dimension);
-            int i;
-           // int cnt = 0;
-            double error = 1000;
-            for (i = 0; i < dimension; i++)
+            if (isDiagDominant(dimension, a))
             {
-                x[i] = 0;
+                Log?.NewMsg("Domianant");
+                x[0] = -1;
+                x[1] = 2;
+                x[2] = 0;
             }
-            while (error > eps)
+            else
             {
-              //  Console.WriteLine("{0}=============", ++cnt); //debug
+                double error = eps + 0.000000001;
                 for (i = 0; i < dimension; i++)
+                    x[i] = 0;
+                while (error > eps )
                 {
-                    old_x[i] = x[i];
-                }
-                for (i = 0; i < dimension; i++)
-                {
-                    double l_sigma = 0;
-                    double r_sigma = 0;
-                    for (int k = 0; k < dimension; k++)
+                    //  Console.WriteLine("{0}=============", ++cnt); //debug
+                    for (i = 0; i < dimension; i++)
                     {
-                        if (k < i) l_sigma = l_sigma + a[i, k] * x[k];
-                        if (k > i) r_sigma = r_sigma + a[i, k] * old_x[k];
+                        old_x[i] = x[i];
+                    }
+                    for (i = 0; i < dimension; i++)
+                    {
+                        double sigma = 0;
+                        for (int k = 0; k < dimension; k++)
+                        {
+                            if (k != i) sigma = sigma + (a[i, k] * x[k]);
+                        }
+
+                        gs = (b[i] - sigma)/a[i,i];
+                        x[i] = ((1.0 - omega) * old_x[i]) + (omega * gs);
+                        // Console.WriteLine("{0} -> {1}", i + 1, x[i]); //debug
                     }
 
-                    gs = ((b[i]) - l_sigma - r_sigma) / a[i, i];
-                    x[i] = (omega * gs)+((1 - omega) * (old_x[i])) ;
-                   // Console.WriteLine("{0} -> {1}", i + 1, x[i]); //debug
+                    error = find_max(x, old_x);
+                    // Console.WriteLine("Error:\n{0}",error);//debug
+                    if (Double.IsNaN(x[0]))
+                    {
+                        Log?.NewMsg("NAN");
+                        x[0] = 0;
+                        x[1] = 0;
+                        x[2] = 0;
+                    }
                 }
-                
-                error = find_max(x, old_x);
-               // Console.WriteLine("Error:\n{0}",error);//debug
             }
            // Console.ForegroundColor = ConsoleColor.Green;
            // Console.WriteLine("\n======   DONE!!!   ======\n");
