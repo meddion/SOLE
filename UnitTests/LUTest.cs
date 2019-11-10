cdc5f06ffdf602faa493799e517d5f8879c609ac
kazbannazar <kazbannazar@gmail.com>
2019-11-10 17:44:09 +0200

    added test for LU

---
+++ UnitTests/LUTest.cs
@@ -0,0 +1,27 @@
+﻿using Core.Methods;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class LUTest
+    {
+        [Test]
+        public void RandomMatrixTest()
+        {
+            var lu = new LUmet();
+            Assert.IsTrue(MethodTest.RandomMatrixTest(lu));
+        }
+        [Test]
+        public void KnowedMatrixTest()
+        {
+            var lu = new LUmet();
+            Assert.IsTrue(MethodTest.KnowedMatrixTest(lu));
+        }
+    }
+}
